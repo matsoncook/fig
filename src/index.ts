b608875db79c944c1b977a083fd9ac88f901014b
matsoncook <markcook2001@gmail.com>
2025-08-06 07:43:32 +0000

    server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import LoadHandler from "./loader/LoadHandler";
 import SwirlLoader from "./loader/SwirlLoader";
 
 const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
+if(ctx)
+{
+  let loader = new LoadHandler(canvas, ctx, new SwirlLoader());
+  loader.setSize(window.innerWidth, window.innerHeight);
+  loader.drawLoadingProgress();
+}
 
-let sw = new SwirlLoader();
-sw.drawSwirl();
+
