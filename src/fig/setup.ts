00e1bd6a1aa067201e4192dd6870050c1a1b78da
matsoncook <markcook2001@gmail.com>
2025-08-09 05:13:02 +0000

    setup game

---
+++ src/fig/setup.ts
@@ -0,0 +1,24 @@
+import Game from "../template/gamejs/Game";
+
+function setupGame(canvas:any,controlCanvas : any,audio :any)
+{
+  //testLineSegmentIntersecter();
+  var game = new Game();
+/*
+  game.gameRenderer.canvas = canvas;
+  game.gameControl.controlCanvas = controlCanvas;	
+  game.gameRenderer.setSize(window.innerWidth,window.innerHeight);
+
+  game.gameRenderer.context = canvas.getContext('2d');
+  game.gameRenderer.staticImageCount = 8;
+  game.gameRenderer.staticImages = [];
+  for(var i = 0;i<game.gameRenderer.staticImageCount;i++)
+  {
+    game.gameRenderer.staticImages[i] = new Image();
+  }
+  for(var i = 0;i<game.gameRenderer.staticImageCount;i++)
+  {
+    game.gameRenderer.staticImages[i].src="images/flowers/download0"+i+".png";
+  }
+  */
+}
