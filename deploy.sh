597b937e9e8a260d96e3c987639653fd985cad12
Mark Cook <markcook2001@gmail.com>
2025-08-24 21:26:16 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
-cp fig.html /home/ubuntu/greycube5/src/main/webapp/fig1
-cp fig.html /home/ubuntu/greycube5/src/main/webapp/fig1/index.html
+#export DEPLOY_DIR=/home/ubuntu/greycube5/src/main/webapp
+export DEPLOY_DIR=/var/www/html
+
+export SUDO=sudo
+
+${SUDO} mkdir ${DEPLOY_DIR}/fig1
+
+#${SUDO} cp fig.html ${DEPLOY_DIR}/fig1
+${SUDO} cp fig.html ${DEPLOY_DIR}/fig1/index.html
 
 
-cp fig.html /home/ubuntu/greycube5/src/main/webapp/fig1
-cp fig.html /home/ubuntu/greycube5/src/main/webapp/fig1/index.html
+${SUDO} mkdir ${DEPLOY_DIR}/fig1/dist
+${SUDO} cp dist/fig_bundle.js ${DEPLOY_DIR}/fig1/dist
 
-mkdir /home/ubuntu/greycube5/src/main/webapp/fig1/dist
-cp dist/bundle.js /home/ubuntu/greycube5/src/main/webapp/fig1/dist
+${SUDO} mkdir ${DEPLOY_DIR}/fig1/images
 
-mkdir /home/ubuntu/greycube5/src/main/webapp/fig1/images
-
-cp -r images/* /home/ubuntu/greycube5/src/main/webapp/fig1/images
+${SUDO} cp -r images/* ${DEPLOY_DIR}/fig1/images
