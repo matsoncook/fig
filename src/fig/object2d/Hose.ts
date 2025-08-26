9e306d6b10c8bde1354318371ba0190894f364c4
Mark Cook <markcook2001@gmail.com>
2025-08-26 00:03:04 +0000

    hose

---
+++ src/fig/object2d/Hose.ts
@@ -0,0 +1,39 @@
+import ImageObject from "../../object2d/ImageObject";
+import GameRenderer from "../../template/gamejs/GameRenderer";
+export default class Hose extends ImageObject{
+    
+
+    render(gameRenderer : GameRenderer)
+            {
+                if(this.cull)return;
+                var posX = gameRenderer.gameToCanvasX(this.position.x);
+                var posY = gameRenderer.gameToCanvasY(this.position.y);
+                var sizeX = gameRenderer.gameToCanvasSizeX(this.size.x);
+                var sizeY = gameRenderer.gameToCanvasSizeY(this.size.y);
+                var context = gameRenderer.context;
+                var halfSizeX = sizeX/2;
+                var halfSizeY = sizeY/2;
+                
+                //This could be encapsulated into its own class
+                context.save();
+                
+                //var rad = this.velocity.x / this.velocity.y;
+                var rad = Math.atan2(this.velocity.y, this.velocity.x);
+                
+                rad = Math.PI/2 - rad;
+                //context.rotate(degrees*Math.PI/180);
+                context.translate(posX,posY);
+                context.rotate(rad);
+                
+                context.translate(-halfSizeX,-halfSizeY);
+                
+                
+                context.drawImage(this.mcImage!.image,0,0,sizeX,sizeY)
+                
+                context.restore();
+                
+                posX = gameRenderer.gameToCanvasX(this.position.x);
+                posY = gameRenderer.gameToCanvasY(this.position.y);
+                
+            }
+}
