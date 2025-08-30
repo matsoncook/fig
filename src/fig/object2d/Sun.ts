e4a6847d82965c4a4c30ef046aae589ed350668f
Mark Cook <markcook2001@gmail.com>
2025-08-30 20:12:56 +1200

    stepper

--- src/fig/object2d/Sun.ts
+++ src/fig/object2d/Sun.ts
@@ -2,11 +2,15 @@ import McImage from "../../image/McImage";
 import ImageObject from "../../object2d/ImageObject";
 import GameRenderer from "../../template/gamejs/GameRenderer";
 import WorldObjectType from "../../template/gamejs/WorldObjectType";
+import Stepper from "../../transform/Stepper";
 
 export default class Sun extends ImageObject
 {
 
     rotate = 0;
+    pathStepperX = new Stepper(0,0.5,.01);
+    pathStepperY = new Stepper(0,0.5,.01);
+
     constructor()
     {
         super(WorldObjectType.ImageObject,"Sun");
@@ -14,8 +18,16 @@ export default class Sun extends ImageObject
         this.size.set(.20,.20);
     }
 
+    advance(time: number): void {
+        
+    }
+
     render(gameRenderer: GameRenderer)
       {
+
+        this.position.x = -this.pathStepperX.next();
+        this.position.y = this.pathStepperY.next();
+
         var context = gameRenderer.context;
         var posX = gameRenderer.gameToCanvasX(this.position.x);
         var posY = gameRenderer.gameToCanvasY(this.position.y);
@@ -39,7 +51,7 @@ export default class Sun extends ImageObject
             context.save();
 
             context.translate(posX,posY);
-            this.rotate +=0.1;
+            this.rotate +=0.01;
             context.rotate(this.rotate);
             context.translate(-sizeX/2,-sizeY/2);
 
--- src/transform/Stepper.ts
+++ src/transform/Stepper.ts
@@ -1,30 +1,53 @@
 
 
-export default class Stepper
- { 
+export default class Stepper {
 
-   current = 0;
-   multiplier = 1;
-   constructor(private from : number,private to : number,private step: number)
-   {
-     this.current = from;
-   }
-   next()
-   {
-     this.current += this.step * this.multiplier;
-     if(this.current>this.to)
-     {
-       this.current = this.to;
-       this.multiplier = -1;
-     }
-     if(this.current<this.from)
-     {
-       this.current = this.from;
-       this.multiplier = 1;
-     }
-     return this.current; 
-   }
+  current = 0;
+  multiplier = 1;
+  gotToTo = false;
+  gotToFrom = false;
+  constructor(private from: number, private to: number, private step: number) {
+    this.current = from;
   }
+  next() {
+    this.gotToTo = false;
+    this.gotToFrom = false;
+    this.current += this.step * this.multiplier;
+    if (this.current > this.to) {
+      this.current = this.to;
+      this.multiplier = -1;
+      this.gotToTo = true;
+    }
+    if (this.current < this.from) {
+      this.current = this.from;
+      this.multiplier = 1;
+      this.gotToFrom = true;
+    }
+    return this.current;
+  }
+  pause()
+  {
+    this.multiplier = 0;
+  }
+  continue(multiplier = 1)
+  {
+    this.multiplier = multiplier;
+  }
+  checkGotToTo(callback : (stepper : Stepper)=>void)
+  {
+    if(this.gotToTo)
+    {
+      callback(this);
+    }
+  }
+  checkGotToFrom(callback : (stepper : Stepper)=>void)
+  {
+    if(this.gotToFrom)
+    {
+      callback(this);
+    }
+  }
+}
 
 /*
 function Stepper(from,to,step)
