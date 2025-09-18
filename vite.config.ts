f8a907d731351f64779aadbc97d62546438c30eb
Mark Cook <markcook2001@gmail.com>
2025-09-18 19:02:32 +1200

    vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
+  publicDir: 'images',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
